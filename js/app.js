5e132ea8a335f88783367dc8effe213c1034bfdf
Estelle NOE <estelle.noe@telecom-paristech.fr>
2016-03-14 16:12:08 +0100

    Ajout des fichiers Model.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+	// this is the instance of our main model
+	// this is what you should use in your application
+	var model = new Model();
+
+	// //And create the needed controllers and views
+	// var exampleView = new ExampleView($("#exampleView"),model);
+	// var exampleViewController = new ExampleViewController(exampleView,model);
+	
+	
+
+});
