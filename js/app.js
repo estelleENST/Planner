eb38e2dabccf898d62493b721b388a15318e5a36
Céline Chen <celine.chen@telecom-paristech.fr>
2016-03-16 07:54:13 +0100

    Changing the layout style

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 $(function() {
+
+	$('#timepicker1').timepicker();
 	// this is the instance of our main model
 	// this is what you should use in your application
 	var model = new Model();
-
-	model.addDay();
-	model.addActivity(new Activity("Group work",20,1,"IVIS16"));
-	model.addActivity(new Activity("Fika",120,2,"Pink Crush"));
-	
 
 	// //And create the needed controllers and views
 	var exampleView = new ExampleView($("#exampleView"),model);
 	//var exampleViewController = new ExampleViewController(exampleView,model);
 	
-	
+	model.addDay();
+	model.addActivity(new Activity("Group work",20,1,"IVIS16"));
+	model.addActivity(new Activity("Fika",120,2,"Pink Crush"));	
+	model.addActivity(new Activity("Presentation",10,3,"Pink Crush"));	
+	model.addActivity(new Activity("Brainstorm",20,4,"Pink Crush"));	
+
 });
--- js/view/exampleView.js
+++ js/view/exampleView.js
@@ -7,10 +7,10 @@ var ExampleView = function (container, model) {
 	this.updateView1 = function(args) {
 		var tableau = "";
 		model.parkedActivities.forEach(function(element, index, array) {
-			tableau += "<tr><th>" + element.getLength() + " min</th><th class=\"activity\">" + element.getName() + "</th><td>";
+			tableau += "<tr width='100%'><td width='30%' class=\"time\">" + element.getLength() 
+			+ " min</td><td width='70%' class=\"activity type-" + element.getTypeId() + " \">" + element.getName() + "</td></tr>";
 		});
-
-		$("#table-draggable1").html(tableau);
+		$("#parkedDraggable").html(tableau);
 	}
 
 	model.addObserver(this.updateView1);
